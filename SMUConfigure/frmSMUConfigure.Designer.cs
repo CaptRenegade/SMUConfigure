e9abf3174ff5c0ee60a5356ddbacadde67df9e00
CaptRenegade <71333950+CaptRenegade@users.noreply.github.com>
2022-02-06 08:15:51 -0500

    Add project files.

---
+++ SMUConfigure/frmSMUConfigure.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace SMUConfigure
+{
+    partial class frmSMUConfigure
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPathStrings = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbPathStrings
+            // 
+            this.lbPathStrings.FormattingEnabled = true;
+            this.lbPathStrings.ItemHeight = 15;
+            this.lbPathStrings.Location = new System.Drawing.Point(12, 12);
+            this.lbPathStrings.Name = "lbPathStrings";
+            this.lbPathStrings.Size = new System.Drawing.Size(393, 139);
+            this.lbPathStrings.TabIndex = 1;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(249, 180);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(330, 180);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 180);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // frmSMUConfigure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 214);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lbPathStrings);
+            this.Name = "frmSMUConfigure";
+            this.Text = "Configure SiteMap Updater Service";
+            this.Load += new System.EventHandler(this.frmSMUConfigure_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbPathStrings;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ SMUConfigure/frmSMUConfigure.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SMUConfigure
+{
+    public partial class frmSMUConfigure : Form
+    {
+        static string path = "C:\\SiteMapUpdater\\settings.config";
+
+        AESEncryption AES = new AESEncryption();
+
+        public frmSMUConfigure()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSMUConfigure_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(path))
+            {
+                foreach (string line in File.ReadAllLines(path))
+                {
+                    lbPathStrings.Items.Add(AES.Decrypt(line));
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            File.Delete(path);
+
+            StreamWriter sw = File.CreateText(path);
+            {
+                if (lbPathStrings.Items.Count != 0)
+                {
+                    foreach (string item in lbPathStrings.Items)
+                    {
+                        sw.WriteLine(AES.Encrypt(item));
+                    }
+                    sw.Close();
+                }
+                else
+                {
+                    sw.Close();
+                    File.Delete(path);
+                }
+            }
+            Application.Exit();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+
+            DialogResult result = FBD.ShowDialog();
+            {
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(FBD.SelectedPath))
+                {
+                    lbPathStrings.Items.Add(FBD.SelectedPath.ToString());
+                }
+            }
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lbPathStrings.Items.Count > 0)
+            {
+                if (lbPathStrings.SelectedIndex > -1)
+                {
+                    lbPathStrings.Items.RemoveAt(lbPathStrings.SelectedIndex);
+                }
+            }
+        }
+
+
+    }
+}
